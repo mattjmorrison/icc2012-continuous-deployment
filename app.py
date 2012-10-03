17bb864519266575b67cca213d1021c7a8fc54a4
mattjmorrison <mattj.morrison@gmail.com>
2012-10-02 23:58:45 -0500

    tweaks

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 
-@app.route('/')
-def root():
-    return Response("hi!")
+@app.route('/<int:x>/')
+def root(x):
+    if x > 5:
+        text = "Hi!"
+    else:
+        text = "Low :("
+    return Response(text)
+
+
+if __name__ == '__main__':
+    app.run()
